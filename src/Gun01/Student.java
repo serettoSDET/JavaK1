a87727d94187d893d81b8f77370c6fc32e34f2b7
seretto <92294418+seretto@users.noreply.github.com>
2023-02-10 17:09:35 +0100

    2 ok

---
+++ src/Gun01/Student.java
@@ -0,0 +1,17 @@
+package Gun01;
+
+import java.util.Scanner;
+
+public class Student {
+
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba");
+
+        System.out.println("task 2");
+
+        }
+
+    }
+
+
